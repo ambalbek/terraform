30386d3f56677c19c3ade819626768d4d9e8db01
ambalbek <azal88@yahoo.com>
2020-07-29 00:37:29 -0500

    updated

--- iam_user/iam_create.tf
+++ iam_user/iam_create.tf
@@ -2,6 +2,6 @@ provider "aws" {
     region = "us-east-1"
 }
 resource "aws_iam_user" "example" {
-  count = 10#"${length(var.usernames)}"
-  name = "terraform.${count.index+1}"
+  count = "${length(var.usernames)}"
+  name = "${element(var.usernames,count.index )}"
 }
